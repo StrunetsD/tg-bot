d220af5be74aba3777e61273909c8b2ebc1ffad2
StrunetsD <d.strunetss@gmail.com>
2025-01-27 18:49:32 +0300

    main function of download track and create filenamecollectroPP for downloading

--- app/keyboards.py
+++ app/keyboards.py
@@ -4,19 +4,7 @@ from aiogram.utils.keyboard import (ReplyKeyboardBuilder, InlineKeyboardBuilder,
                                     InlineKeyboardMarkup, InlineKeyboardButton)
 
 
-# main = InlineKeyboardMarkup(
-#     inline_keyboard=[
-#         [
-#             InlineKeyboardButton(text='Добавить песню', callback_data='add_music'),
-#             InlineKeyboardButton(text='Создать плейлист', callback_data='add_playlist'),
-#             InlineKeyboardButton(text='Добавить песню в плейлист', callback_data='add_music_to_playlist'),
-#             InlineKeyboardButton(text='Новинки', callback_data='new'),
-#             InlineKeyboardButton(text='Популярное', callback_data='popular'),
-#         ]
-#     ],
-# )
-
-commands = ['Скачать трек','Создать плейлист','Добавить трек в плейлист','Новинки', 'Популярное']
+commands = ['Скачать трек','Плейлист','Создать плейлист','Добавить трек в плейлист','Новинки', 'Популярное']
 
 async def start_commands():
     keyboard = InlineKeyboardBuilder()
