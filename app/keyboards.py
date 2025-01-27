dc4902e49b79194fc43d788f15e72093a0bb4af8
StrunetsD <d.strunetss@gmail.com>
2025-01-27 14:51:42 +0300

    create basic keyboard

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,5 +1,25 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
+                           InlineKeyboardMarkup, InlineKeyboardButton)
+from aiogram.utils.keyboard import (ReplyKeyboardBuilder, InlineKeyboardBuilder,
+                                    InlineKeyboardMarkup, InlineKeyboardButton)
 
-main = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='TEST')]
-],resize_keyboard=True)
+
+# main = InlineKeyboardMarkup(
+#     inline_keyboard=[
+#         [
+#             InlineKeyboardButton(text='Добавить песню', callback_data='add_music'),
+#             InlineKeyboardButton(text='Создать плейлист', callback_data='add_playlist'),
+#             InlineKeyboardButton(text='Добавить песню в плейлист', callback_data='add_music_to_playlist'),
+#             InlineKeyboardButton(text='Новинки', callback_data='new'),
+#             InlineKeyboardButton(text='Популярное', callback_data='popular'),
+#         ]
+#     ],
+# )
+
+commands = ['Скачать трек','Создать плейлист','Добавить трек в плейлист','Новинки', 'Популярное']
+
+async def start_commands():
+    keyboard = InlineKeyboardBuilder()
+    for comm in commands:
+        keyboard.add(InlineKeyboardButton(text=comm, callback_data=comm))
+    return keyboard.adjust(1).as_markup()
