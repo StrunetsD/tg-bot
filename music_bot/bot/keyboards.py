c9685d33d252d460c26d244485b63c3e755b8428
Dmitry Strunets <d.strunetss@gmail.com>
2025-02-04 17:37:19 +0300

    fix :)

--- music_bot/bot/keyboards.py
+++ music_bot/bot/keyboards.py
@@ -1,7 +1,7 @@
 from urllib.parse import quote
 
 from aiogram.utils.keyboard import (InlineKeyboardBuilder, InlineKeyboardButton)
-from music_bot.spotify.get_popular_tracks import search_playlist_tracks
+
 
 commands = ['Скачать трек']
 
@@ -13,23 +13,3 @@ async def start_commands():
     return keyboard.adjust(1).as_markup()
 
 
-def truncate_track_name(track_name):
-    return track_name[:64]
-
-
-async def playlist_command():
-    tracks = await search_playlist_tracks()
-    keyboard = InlineKeyboardBuilder()
-
-    if tracks:
-        for track_name, track_artists, track_id in tracks:
-            display_name = truncate_track_name(f"{track_name} by {track_artists}")
-            safe_track_id = quote(track_id)
-            print(f"Добавляем кнопку с callback_data: {safe_track_id}")
-
-            if len(safe_track_id) > 64:
-                print(f"Длина callback_data превышает 64 байта: {safe_track_id}")
-
-            keyboard.add(InlineKeyboardButton(text=display_name, callback_data=safe_track_id))
-
-    return keyboard.as_markup()
